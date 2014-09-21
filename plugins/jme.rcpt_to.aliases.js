8804d467605b66aaf15083f820aab1a64dcd121d
jme <jinux@alluha.net>
2014-09-21 18:34:58 +0100

    Improved code layout.

--- plugins/jme.rcpt_to.aliases.js
+++ plugins/jme.rcpt_to.aliases.js
@@ -2,42 +2,45 @@
 // Description: Allows arbitrary aliases for addresses
 var Address = require('./address').Address;
 
-function _getAliases(plugin, connection, host, config) {
-	// Where should we look for our accounts?
-	var authMethod = config['authMethod'] || "file";
-	if (authMethod == "file") {
-		// Get our account list
-		if (config['aliases']) {
-			if (config['aliases'][host]) {
-				// We found our host in the list
-				return config['aliases'][host];
-			} else {
-				connection.logdebug(plugin, "Host " + host + " has no aliases configured");
-				return false;
-			}
-		} else {
-			// This shouldn't happen but it might
-			connection.logdebug(plugin, "No aliases table in config file");
-			return false;
-		}
-	}
+function addAddressToTransaction(connection, address) {
+	connection.transaction.rcpt_to.pop();
+	connection.transaction.rcpt_to.push(new Address('<' + address + '>'));
 }
 
-function _resolveAlias(plugin, connection, user, host, config) {
-	// Get the aliases for the given host
-	var localAliases = _getAliases(plugin, connection, host, config);
-	var userTo = user,
-		addressTo = host,
-		bottomAddress = user + "@" + host;
-	if (localAliases && localAliases[user]) {
-		// Out alias will either be ['name'] or ['name', 'host']
-		var wholeAliasTo = localAliases[user].split("@", 2);
-		userTo = wholeAliasTo[0];
-		if (wholeAliasTo[1]) {
-			addressTo = wholeAliasTo[1];
+function setParams(params, address) {
+	params[0].user = address.split("@", 2)[0];
+	params[0].host = address.split("@", 2)[1];
+}
+
+function getAliases(plugin, connection, config) {
+	var authMethod = config['authMethod'] || "file";
+	var aliases = false;
+	if (authMethod == "file") {
+		connection.logdebug(this, "Using file-auth");
+		if (config['aliases']) {
+			aliases = config['aliases'];
+		} else {
+			connection.logdebug(plugin, "No aliases table in config file");
 		}
-		// Resolve the new alias we found.
-		bottomAddress = _resolveAlias(plugin, connection, userTo, addressTo, config);
+	}
+	return aliases;
+}
+
+function resolveAlias(plugin, connection, user, host, config) {
+	var aliasTo;
+	var localAliases = config[host];
+	var bottomAddress = user + "@" + host;
+	if (localAliases && localAliases[user]) {
+		if (bottomAddress == localAliases[user]) {
+			connection.logdebug(plugin, "Cyclic alias found, stopping here: " + bottomAddress);
+		} else {
+			aliasTo = localAliases[user].split("@", 2);
+			user = aliasTo[0];
+			if (aliasTo[1]) {
+				host = aliasTo[1];
+			}
+			bottomAddress = resolveAlias(plugin, connection, user, host, config);
+		}
 	}
 	return bottomAddress;
 }
@@ -46,26 +49,20 @@ exports.hook_rcpt = function(next, connection, params) {
 	var config      = this.config.get('jme.rcpt_to.aliases', 'json') || {};
 	var rcpt        = params[0];
 	var address     = rcpt.address();
-	// We don't do any '-' matching on this address
 	var user        = rcpt.user;
 	var host        = rcpt.host;
 
 	connection.loginfo(this, "Checking aliases for: " + address);
 	var addressBefore = user + "@" + host;
-	var addressAfter = _resolveAlias(this, connection, user, host, config);
+	var aliases = getAliases(this, connection, config);
+	var addressAfter = resolveAlias(this, connection, user, host, aliases);
 
 	if (addressBefore == addressAfter) {
-		// No aliases happened
 		connection.logdebug(this, "Found no aliases for: " + addressBefore);
-		return next();
 	} else {
-		// Successful alias
 		connection.logdebug(this, addressBefore + " aliases to " + addressAfter);
-		params[0].user = addressAfter.split("@", 2)[0];
-		params[0].host = addressAfter.split("@", 2)[1];
-		// Stick it back on the 'true' array, not just the params
-		connection.transaction.rcpt_to.pop();
-		connection.transaction.rcpt_to.push(new Address('<' + addressAfter + '>'));
+		setParams(params, addressAfter);
+		addAddressToTransaction(connection, addressAfter);
 	}
 	return next();
 };
