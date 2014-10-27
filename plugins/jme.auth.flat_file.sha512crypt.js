592e80f825a677aa6073e376623e9ea7cf4e2c42
jme <jinux@alluha.net>
2014-10-27 23:25:49 +0000

    [Plugin] Added 'jme.auth.flat_file.sha512crypt' plugin!
    This plugin allows you to store user passwords hashed with SHA512-CRYPT mechanism, instead of cleartext.
    
    It integrates with the other 'jme' modules to provide inline configuration as well.

---
+++ plugins/jme.auth.flat_file.sha512crypt.js
@@ -0,0 +1,60 @@
+// jme.auth.flat_file_sha512-crypt.js
+
+var sha512 = require('sha512crypt-node');
+
+exports.register = function () {
+	var plugin = this;
+	plugin.inherits('auth/auth_base');
+	var load_config = function () {
+		plugin.cfg = plugin.config.get('jme.mailbox_accounts', 'json') || {};
+	};
+	load_config();
+};
+
+exports.hook_capabilities = function (next, connection) {
+	var plugin = this;
+	// don't allow AUTH unless private IP or encrypted
+	if (!connection.using_tls) {
+		connection.logdebug(plugin, "Auth disabled for insecure public connection");
+		return next();
+	}
+
+	var methods = ['PLAIN', 'LOGIN'];
+
+	connection.capabilities.push('AUTH ' + methods.join(' '));
+	connection.notes.allowed_auth_methods = methods;
+
+	next();
+};
+
+exports.retrieveUser = function (user, cb) {
+	var plugin = this;
+	var username = user.split('@')[0];
+	var domain = user.split('@')[1] || '';
+	connection.loginfo(plugin, "Checking password for " + user);
+	if (plugin.cfg.accounts[domain]) {
+		if (plugin.cfg.accounts[domain][username]) {
+			return cb(plugin.cfg.accounts[domain][username].password);
+		}
+	}
+	return cb(null);
+};
+
+exports.validateHash = function(hash, password) {
+	var salt = hash.match(/^\$6\$(.*)\$/)[1];
+	var validHash = sha512.b64_sha512crypt(password, salt);
+	if (validHash === hash) return true;
+	return false;
+};
+
+exports.check_plain_passwd = function (connection, user, password, cb) {
+	var instance = this;
+	this.retrieveUser(user, function (hash) {
+		if (hash === null) {
+			return cb(false);
+		}
+
+		var validHash = instance.validateHash(hash, password);
+		return cb(validHash);
+	});
+};
